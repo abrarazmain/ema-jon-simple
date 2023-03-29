36e2132989b686130933d4f1e2c1448e144b9aa5
abrarazmain <ajmainabrar5@gmail.com>
2023-03-29 23:51:52 +0600

    added localstore

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Cart from "../cart/Cart";
 import Product from "../product/Product";
 import "./Shop.css";
+import {addToDb, getShoppingCart} from '../../utilities/fakedb'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -13,10 +14,17 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
     
+  useEffect(() => {
+    const storedCart = getShoppingCart()
+    console.log(storedCart);
+},[])
+
   const handleAddToCart = (product) => {
       console.log(product);
       const newCart = [...cart, product]
-      setCart(newCart)
+    setCart(newCart)
+    addToDb(product.id)
+    
 }
 
   return (
