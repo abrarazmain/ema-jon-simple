a0c5854822231226a554c7d5885f6966eae3f6fb
abrarazmain <ajmainabrar5@gmail.com>
2023-03-29 15:08:17 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Shop from "./components/shop/Shop";
 
 function App() {
   return <div className="App">
-<Header></Header>
+    <Header></Header>
+    <Shop></Shop>
 
   </div>;
 }
